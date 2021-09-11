27d7e45bfbb4a14c2b8387c660a8b80d4e594bc9
MaramB <maram.badr1996@gmail.com>
2021-09-11 23:12:58 +0200

    angular build

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const compression = require('compression')
+const express = require("express");
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(__dirname + '/angular-build'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
